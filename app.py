b73a7caf35f87d5eb44563f0e3e825fe5385127e
73Dillon <dillonsd18@gmail.com>
2024-08-23 16:43:55 -0600

    commit
    implementing Dot AI suggestion to fix ArrowTypeError

--- app.py
+++ app.py
@@ -8,12 +8,8 @@ import streamlit as st
 #read in csv
 df = pd.read_csv('new_vehicles_us.csv')
 
-# Check for null values in 'price' column and handle them
-if df['price'].isnull().any():
-    df['price'].fillna(0, inplace=True)
-
-# Ensure 'price' column is of integer type
-df['price'] = df['price'].astype(int)
+# Set Streamlit to use pandas engine explicitly
+st.experimental_set_dataframe_engine(engine="pandas")
 
 #title
 st.title('Car Sales Listings in the U.S.')
